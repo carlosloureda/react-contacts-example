8729596e17fac423c439ea116ded34f109c3858e
Carlos Loureda <carlos.loureda@domesting.com>
2017-11-11 13:36:03 +0100

    Added list contacts with mocked data (module 1.2)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ListContacts from './ListContacts';
 
-class ContactList extends React.Component {
-  render() {
-    const people  = this.props.contacts
-
-    return <ol>
-      {people.map(person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
   }
-}
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'Michael' },
-          { name: 'Ryan' },
-          { name: 'Tyle' },
-        ] }/>
-        <ContactList contacts={[
-          { name: 'Amanda' },
-          { name: 'Richard' },
-          { name: 'Geoff' },
-        ] }/>
+      <div>
+        <ListContacts contacts={contacts}/>
       </div>
-    );
-  }
+    )
+  }  
 }
-
 export default App;
